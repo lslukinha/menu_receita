828998b3255ad2f2ace954945279cfce8dc69607
lucas rodrigues <lrc@cesar.school>
2024-05-15 15:36:19 -0300

    adicionei o arquivo para ser armazenar a função add_receita e visualizarReceitas  em txt

--- menu.py
+++ menu.py
@@ -1,19 +1,20 @@
-def add_receita(receitas_array):
+
+def add_receita():
+    file = open("menu.txt","a")
     nome = input("Digite o nome da receita : ")
     pais_origem = input("Digite o país de origem da receita : ")
     ingredientes = input("Digite os ingredientes da receita separados por ';' : ")
     preparo = input("Digite o modo de preparo da receita separado por ';' : ")
-    novaReceita = {"nome": nome,"pais_origem": pais_origem,"ingredientes": ingredientes.split(';'),"preparo": preparo.split(';')}
-    receitas_array.append(novaReceita)
+    novaReceita =  f"{nome}, {pais_origem}, {ingredientes}, {preparo}\n"
+    print(novaReceita)
+    file.write(novaReceita)
+    file.close()
     print("Nova receita adicionada com sucesso!")
+
 def visualizarReceitas(receitas_array):
-    for receita in receitas_array:
-        print(f"Nome: {receita['nome']}")
-        print(f"Pais de origem: {receita['pais_origem']}")
-        print("Ingredientes:", receita['ingredientes'])
-        print("Preparo:", receita['preparo'])
-        print()
-receitas_array=[]
+    file = open("menu.txt","r")
+    file.close()
+
 while True:
     print("\nMenu:")
     print("1- Adicionar receita")
@@ -24,6 +25,6 @@ while True:
     escolha = input("Escolha a opção desejada(1-5): ")
 
     if escolha == "1":
-        add_receita(receitas_array)
+         add_receita()
     elif escolha =="4":
-        visualizarReceitas(receitas_array)
+        visualizarReceitas()
