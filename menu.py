41e1d3c67e66d4f34a2cbf9495a44170b8e88a70
davicas2 <daaviisantiago@gmail.com>
2024-05-15 21:56:43 -0300

    adicionei a função de atualizar a receita

--- menu.py
+++ menu.py
@@ -1,4 +1,3 @@
-
 def add_receita():
     file = open("menu.txt","a")
     nome = input("Digite o nome da receita : ")
@@ -16,6 +15,37 @@ def visualizarReceitas(novaReceita):
     file.write(novaReceita)
     file.close()
 
+def att_receita():
+    file = open("menu.txt","r")
+    file.write(novaReceita)
+    nome_antigo = input("Digite o nome da receita que deseja atualizar: ")
+    encontrou = False
+
+    with open("menu.txt", "r") as file:
+        linhas = file.readlines()
+
+    with open("menu.txt", "w") as file:
+        i = 0
+        while i < len(linhas):
+            if nome_antigo in linhas[i]:
+                encontrou = True
+                nome_novo = input("Digite o novo nome da receita (ou pressione Enter para manter o atual): ") or linhas[i].split(": ")[1].strip()
+                pais_origem_novo = input("Digite o novo país de origem da receita (ou pressione Enter para manter o atual): ") or linhas[i+1].split(": ")[1].strip()
+                ingredientes_novo = input("Digite os novos ingredientes da receita separados por ';' (ou pressione Enter para manter os atuais): ") or linhas[i+2].split(": ")[1].strip()
+                preparo_novo = input("Digite o novo modo de preparo da receita separado por ';' (ou pressione Enter para manter o atual): ") or linhas[i+3].split(": ")[1].strip()
+                novaReceita = f"Nome da Receita: {nome_novo}\nPaís de origem: {pais_origem_novo}\nIngredientes da Receita: {ingredientes_novo}\nPreparo: {preparo_novo}\n\n"
+                file.write(novaReceita)
+                i += 5  
+            else:
+                file.write(linhas[i])
+                i += 1
+
+    if encontrou:
+        print("Receita atualizada com sucesso!")
+    else:
+        print("Receita não encontrada.")
+
+
 while True:
     print("\nMenu:")
     print("1- Adicionar receita")
@@ -27,5 +57,7 @@ while True:
 
     if escolha == "1":
          add_receita()
+    elif escolha == "2":
+        att_receita()
     elif escolha =="4":
         visualizarReceitas()
